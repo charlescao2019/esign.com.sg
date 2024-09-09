1c0fe03767298516322be4b57dde53a07080942c
Charles Cao <wjcao@Marias-MacBook-Pro-2.local>
2024-09-09 19:06:02 +0800

    adding email with customer names and sender name 4

--- resources/views/notifySigner.blade.php
+++ resources/views/notifySigner.blade.php
@@ -53,7 +53,7 @@
         <!-- Content -->
         <div class="content">
 
-            Dear,
+            Dear {{ $signers->name }},
 
             <!-- Text -->
             <p class="text">You have been invited to sign a document. Please visit the below url.</p>
